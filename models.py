48a9f77b4bb04decba17ccddf13ccd48df58e70e
Anirudh Suresh <anirudhtx@gmail.com>
2019-04-21 13:33:28 -0400

    mechanically implemented rec

--- models.py
+++ models.py
@@ -3,8 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import torch.distributions as D
+import copy
 
 epsilon_std = 1.0
+softmaxfunc = nn.Softmax(-1)
 
 def sampling(z_mean, z_log_var, latent_dim): 
 	epsilon = torch.normal(torch.Tensor([[0.]*latent_dim]*len(z_mean)),std=epsilon_std)
@@ -81,7 +83,7 @@ class VAE_rec(nn.Module):
 		self.layers_dec_post_rec = layers_dec_post_rec
 		self.dec_lin = dec_lin       
 		
-	def forward(self, x):
+	def forward(self, x, train_stat=True):
 		bs = x.shape[0]
 		seq_len = x.shape[1]
 		x_init = x
@@ -120,13 +122,49 @@ class VAE_rec(nn.Module):
 			hid_0 = self.layers_dec[0](x)
 			x = x.transpose(0,1)
 
-			for layer in self.layers_dec[1:]:
-				x,_ = layer(x_init, hid_0)
+			if train_stat: # if training, then do teacher forcing
+				x_pass = x_init[:,:-1,:]
+				x_pass = torch.cat((torch.zeros_like(x_pass[:,0,:]).unsqueeze(-2), x_pass), dim = -2)
+
+				for layer in self.layers_dec[1:]:
+					x,_ = layer(x_pass, hid_0)
+
+			else: # else, do either naive next step prediction or beam search
+				hids = []
+				xs = []
+
+				assert(len(self.layers_dec) == 2), 'Length of layers_dec module list should only be 2 (1 linear & 1 recurrent)'
+
+				for layer in self.layers_dec[1:]: # should only be 1 more layer in this ModuleList
+					predx_t,hid_t = layer(torch.zeros_like(x_init[:,0,:]).unsqueeze(-2), hid_0)
+
+					xs.append(predx_t)
+					hids.append(hid_t)
+
+					for i in range(seq_len-1):
+						# Naive, without beam search
+						inter = hids[-1][-2:].transpose(0,1)
+						inter = inter.contiguous().view(bs,-1)
+						predx_t = self.layers_dec_post_rec[0](inter)
+						for layer_sub in self.layers_dec_post_rec[1:]:
+							predx_t = layer_sub(predx_t)
+							xs.append(predx_t)
+
+						predx_t = softmaxfunc(predx_t)
+
+						_,hid_tplusone = layer(predx_t.unsqueeze(1), hids[-1])
+
+						hids.append(hid_tplusone)
+
+				x = torch.stack(hids)[:,-2:,:,:]
+				x = x.transpose(1,2).contiguous().view(seq_len,bs,-1).transpose(0,1)
+
 
 			for layer in self.layers_dec_post_rec:
 				x = layer(x)
 
 		return x
+
 
 
 			# for layer in self.layers_dec_pre_rec:
